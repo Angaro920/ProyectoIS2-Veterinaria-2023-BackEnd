8c478eca3cd5cf23fd84d72c8ab2a7f6606f8c6b
Angaro920 <95058616+Angaro920@users.noreply.github.com>
2023-04-28 20:52:11 -0500

    Upload Files

---
+++ Veterinaria_Proyecto/Controllers/DashboardController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Veterinaria_Proyecto.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Pets()
+        {
+            return View();
+        }
+        public ActionResult Products()
+        {
+            return View();
+        }
+        public ActionResult ClinicHistory()
+        {
+            return View();
+        }
+        public ActionResult Clients()
+        {
+            return View();
+        }
+    }
+}
